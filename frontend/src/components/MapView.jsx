64f0c90917be96a42e8ae42cacd0fbe4a5703ce9
geeky4dev <geeky4dev@gmail.com>
2025-07-08 12:16:48 +0200

    Edited MapView.jsx to show marker images of Leaftlet

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -1,11 +1,24 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// Arreglo para que se muestren los íconos correctamente en producción
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 function MapView({ site }) {
   if (!site) return null;
 
   return (
-    <MapContainer center={[site.lat, site.lng]} zoom={13} style={{ height: '400px' }}>
+    <MapContainer center={[site.lat, site.lng]} zoom={13} style={{ height: '400px', width: '100%' }}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <Marker position={[site.lat, site.lng]}>
         <Popup>{site.name}</Popup>
@@ -15,3 +28,4 @@ function MapView({ site }) {
 }
 
 export default MapView;
+
