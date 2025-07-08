a392f9d4aed58a3b0b3e82195d1f49498f48dafb
geeky4dev <geeky4dev@gmail.com>
2025-07-08 12:43:01 +0200

    Edited src/components/MapView.jsx to show markers of Leaftlet

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -1,11 +1,14 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
-// Arreglo para que se muestren los íconos correctamente en producción
+// Importar las imágenes NO es necesario si las sirves desde /public/images/
+// Puedes eliminar estas tres líneas si usas rutas absolutas desde public/
+// import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+// import markerIcon from 'leaflet/dist/images/marker-icon.png';
+// import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// Solución para que los íconos se carguen correctamente en producción:
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
@@ -18,7 +21,11 @@ function MapView({ site }) {
   if (!site) return null;
 
   return (
-    <MapContainer center={[site.lat, site.lng]} zoom={13} style={{ height: '400px', width: '100%' }}>
+    <MapContainer
+      center={[site.lat, site.lng]}
+      zoom={13}
+      style={{ height: '400px', width: '100%' }}
+    >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <Marker position={[site.lat, site.lng]}>
         <Popup>{site.name}</Popup>
@@ -28,4 +35,5 @@ function MapView({ site }) {
 }
 
 export default MapView;
+
 
