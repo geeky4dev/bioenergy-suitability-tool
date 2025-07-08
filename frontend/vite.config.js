c5c2c92b4a4334babd88dcfb30436271ba427315
geeky4dev <geeky4dev@gmail.com>
2025-07-08 12:30:27 +0200

    Edited vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.png', '**/*.svg', '**/*.gif'],
   resolve: {
-    alias: {
-      // Alias a carpeta exacta de leaflet para evitar problemas de resolución
-      'leaflet': path.resolve(__dirname, 'node_modules/leaflet/dist/leaflet-src.js'),
-    }
+    // No alias para 'leaflet' para evitar errores de build
   },
-  assetsInclude: ['**/*.png', '**/*.svg', '**/*.gif'], // por si agregas otros assets
-  build: {
-    rollupOptions: {
-      output: {
-        // Esto puede ayudar a manejar rutas relativas de assets
-        assetFileNames: 'assets/[name].[hash][extname]'
-      }
-    }
-  }
 });
